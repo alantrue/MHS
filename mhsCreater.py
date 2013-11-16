20710229c5d269335666051b0ab8a91c44e19a21
alantrue <alantruekimo@yahoo.com.tw>
2013-11-16 21:41:30 +0800

    complete

--- mhsCreater.py
+++ mhsCreater.py
@@ -4,16 +4,40 @@ import os
 import csv
 import shutil
 import zipfile
+import tempfile
+import datetime
+import re
+import random
 
-'''
-files = os.listdir("C:\\Users\\alantrue\\Desktop\\work")
-for filename in files:
-	print(filename)
-'''
 workPath = u'C:/Users/alantrue/Desktop/work/'
 tempPath = u'C:/Users/alantrue/Desktop/work/temp/'
 files = (u'無口語紀錄表.docx', u'無口語評估表.docx', u'詞彙簡單句紀錄表.docx', u'詞彙簡單句評估表.docx', u'構音紀錄表.docx', u'構音評估表.docx', u'複雜句故事敘述紀錄表.docx', u'複雜句故事敘述評估表.docx')
 
+def remove_from_zip(zipfname, *filenames):
+    tempdir = tempfile.mkdtemp()
+    try:
+        tempname = os.path.join(tempdir, 'new.zip')
+        with zipfile.ZipFile(zipfname, 'r') as zipread:
+            with zipfile.ZipFile(tempname, 'w') as zipwrite:
+                for item in zipread.infolist():
+                    if item.filename not in filenames:
+                        data = zipread.read(item.filename)
+                        zipwrite.writestr(item, data)
+        shutil.move(tempname, zipfname)
+    finally:
+        shutil.rmtree(tempdir)
+
+def replaceOccurence(str, search, replacement, index):
+	split = str.split(search, index+1)
+	if len(split) <= index+1:
+		return str
+	return search.join(split[:-1])+replacement+split[-1]
+
+def randomProgress(p):
+	for i in range(len(p)):
+		if p[i] < 5:
+			if random.randint(0,99) < 25:
+				p[i] += 1
 
 def parseInfo():
 	#解析出0病歷號, 1姓名, 2生日, 3初診日, 4性別, 5階段, 6紀錄
@@ -63,33 +87,92 @@ def parseInfo():
 			fileSrc2 = workPath + fileSrc2
 			fileDes1 = tempPath.encode('big5') + fileDes1
 			fileDes2 = tempPath.encode('big5') + fileDes2
-			print row[0], row[1], row[2], row[3], row[4], row[5], row[6]
-			print i, fileSrc1, fileSrc2, fileDes1, fileDes2
+			#print row[0], row[1], row[2], row[3], row[4], row[5], row[6]
+			#print i, fileSrc1, fileSrc2, fileDes1, fileDes2
 			#建立拷貝zip
 			shutil.copyfile(fileSrc1, fileDes1.decode('big5'))
 			shutil.copyfile(fileSrc2, fileDes2.decode('big5'))
-		#解壓縮
-			zf = zipfile.ZipFile(fileDes1.decode('big5'), 'r')
-			data = zf.read('word/document.xml')
-			data = data.decode('utf8')
-		#填入資料
-			#病歷號碼
-			data = data.replace(u'123456', row[0].decode('big5'))
-			#名字
-			data = data.replace(u'我的名字', row[1].decode('big5'))
-			#生日
-			data = data.replace(u'birth', row[2].decode('big5'))
-			#初診日期
-			#男女
 
-			print data
-			#data.encode('utf8')
-			#zf.write(data,'word/document.xml')
-		#壓縮回zip
-		#改回docx
+			createMHS(row, fileDes1)
+			createMHS(row, fileDes2)
 
 		i = i + 1
-		if i > 1:
+		if i > 3:
 			break
+
+def createMHS(row, fileDes):	
+	#解壓縮
+		zfr = zipfile.ZipFile(fileDes.decode('big5'), 'r')
+		data = zfr.read('word/document.xml')
+		data = data.decode('utf8')
+	#填入資料
+		#病歷號碼
+		data = re.sub(r'\b123456\b', row[0].decode('big5'), data)
+
+		#名字
+		replaceLen = len(u'我的名字')
+		nameLen = len(row[1].decode('big5'))
+		replaceName = row[1].decode('big5') + u'　' * (replaceLen - nameLen)
+		data = data.replace(u'我的名字', replaceName)
+
+		#生日
+		birth = row[2]
+		if birth[0] == '0':
+			birth = birth[1:]
+
+		data = re.sub(r'\bbirth\b', birth.decode('big5'), data)
+		#初診日期
+		firstDate = row[3].decode('big5').split('/')
+		data = re.sub(r'\bjkl\b', firstDate[0], data)
+		data = re.sub(r'\bmn\b', firstDate[1], data)
+		data = re.sub(r'\bop\b', firstDate[2], data)
+
+		#男女
+		if row[4] == u'男'.encode('big5'):
+			data = replaceOccurence(data, '<w:default w:val="1"/>', '', 1)
+		elif row[4] == u'女'.encode('big5'):
+			data = replaceOccurence(data, '<w:default w:val="1"/>', '', 0)
+		else:
+			pass
+
+		#紀錄
+		records = row[6].split(',')
+
+		p = [1, 1, 1, 1, 1, 1, 1, 1]
+		for i in range(6):
+			if i < len(records):
+				data = re.sub(r'\b00{0}\b'.format(i+1), records[i], data)
+				data = re.sub(r'\b01{0}\b'.format(i+1), str(p[0]), data)
+				data = re.sub(r'\b02{0}\b'.format(i+1), str(p[1]), data)
+				data = re.sub(r'\b03{0}\b'.format(i+1), str(p[2]), data)
+				data = re.sub(r'\b04{0}\b'.format(i+1), str(p[3]), data)
+				data = re.sub(r'\b05{0}\b'.format(i+1), str(p[4]), data)
+				data = re.sub(r'\b06{0}\b'.format(i+1), str(p[5]), data)
+				data = re.sub(r'\b07{0}\b'.format(i+1), str(p[6]), data)
+				data = re.sub(r'\b08{0}\b'.format(i+1), str(p[7]), data)
+				randomProgress(p)
+			else:
+				data = re.sub(r'\b00{0}\b'.format(i+1), '', data)
+				data = re.sub(r'\b01{0}\b'.format(i+1), '', data)
+				data = re.sub(r'\b02{0}\b'.format(i+1), '', data)
+				data = re.sub(r'\b03{0}\b'.format(i+1), '', data)
+				data = re.sub(r'\b04{0}\b'.format(i+1), '', data)
+				data = re.sub(r'\b05{0}\b'.format(i+1), '', data)
+				data = re.sub(r'\b06{0}\b'.format(i+1), '', data)
+				data = re.sub(r'\b07{0}\b'.format(i+1), '', data)
+				data = re.sub(r'\b08{0}\b'.format(i+1), '', data)
+
+		#刪除舊xml
+		remove_from_zip(fileDes.decode('big5'), 'word/document.xml')
+
+		#寫入新xml
+		zfw = zipfile.ZipFile(fileDes.decode('big5'), 'a')
+		zfw.writestr('word/document.xml', data.encode('utf8'))
+
+		zfr.close()
+		zfw.close()
+	#改回docx
+		filename = os.path.splitext(fileDes.decode('big5'))[0]
+		os.rename(fileDes, filename + ".docx")
 
 parseInfo()
