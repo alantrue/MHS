dda775621716567982736c00cfcadbb8dfbe9ef4
alantrue <alantruekimo@yahoo.com.tw>
2013-11-17 11:18:29 +0800

    multi file for records

--- mhsCreater.py
+++ mhsCreater.py
@@ -8,7 +8,7 @@ import tempfile
 import datetime
 import re
 import random
-import codecs
+import math
 
 workPath = os.getcwd() + '/'
 outputPath = workPath + u'病歷表/'
@@ -38,7 +38,13 @@ def replaceOccurence(str, search, replacement, index):
 def randomProgress(p):
 	for i in range(len(p)):
 		if p[i] < 5:
-			if random.randint(0,99) < 25:
+			if p[i] == 1 and random.randint(0,99) < 25:
+				p[i] += 1
+			elif p[i] == 2 and random.randint(0,99) < 20:
+				p[i] += 1
+			elif p[i] == 3 and random.randint(0,99) < 10:
+				p[i] += 1
+			elif p[i] == 4 and random.randint(0,99) < 5:
 				p[i] += 1
 
 def removeFileInFolder(folder):
@@ -59,7 +65,7 @@ def parseInfo():
 
 		fileSrc1 = u''
 		fileSrc2 = u''
-		fileDes1 = u'治療表.zip'.encode('utf8')
+		fileDes1 = u'治療表{0}.zip'.encode('utf8')
 		fileDes2 =  u'評估表.zip'.encode('utf8')
 		#依據階段複製範本, 並命名病歷號+姓名+(語言or構音)+(治療, 評估)表
    		if row[5] == u'無口語'.encode('utf8'):
@@ -100,21 +106,39 @@ def parseInfo():
 			fileDes2 = outputPath.encode('utf8') + fileDes2
 
 			#建立拷貝zip
+			p = [1, 1, 1, 1, 1, 1, 1, 1]
 			try:
-				shutil.copyfile(fileSrc1, fileDes1.decode('utf8'))
-				shutil.copyfile(fileSrc2, fileDes2.decode('utf8'))
+				needCount = math.ceil(len(row[6].split(',')) / 6.0)			
+				if needCount < 2: #只需一份
+					fileDes1 = fileDes1.format('')
+					shutil.copyfile(fileSrc1, fileDes1.decode('utf8'))
+					#產生紀錄表
+					createMHS(row, fileDes1, p)
+				else:
+					for i in range(int(needCount)):
+						fileDes1Temp = fileDes1.format(i+1)
+						shutil.copyfile(fileSrc1, fileDes1Temp.decode('utf8'))
+						#產生紀錄表
+						createMHS(row, fileDes1Temp, p, i*6)
+
 			except IOError as e:
-				print u'無法產生 {0} 的病歷表'.encode('big5').format(row[0])
+				print u'無法產生 {0} 的紀錄表'.encode('big5').format(row[0])
 				print u'進行中'.encode('big5')
 				continue
 
-			#產生病歷表
-			createMHS(row, fileDes1)
-			createMHS(row, fileDes2)
+			#建立拷貝zip
+			try:
+				shutil.copyfile(fileSrc2, fileDes2.decode('utf8'))
+				#產生評估表
+				createMHS(row, fileDes2)
+			except IOError as e:
+				print u'無法產生 {0} 的評估表'.encode('big5').format(row[0])
+				print u'進行中'.encode('big5')
+				continue
 
 		i = i + 1
 
-def createMHS(row, fileDes):	
+def createMHS(row, fileDes, p=None, recodeIndex=0):	
 	#解壓縮
 		zfr = zipfile.ZipFile(fileDes.decode('utf8'), 'r')
 		data = zfr.read('word/document.xml')
@@ -150,32 +174,32 @@ def createMHS(row, fileDes):
 		else:
 			pass
 
-		#紀錄
-		records = row[6].split(',')
+		if p is not None:
+			#紀錄
+			records = row[6].split(',')
 
-		p = [1, 1, 1, 1, 1, 1, 1, 1]
-		for i in range(6):
-			if i < len(records):
-				data = re.sub(r'\b00{0}\b'.format(i+1), records[i], data)
-				data = re.sub(r'\b01{0}\b'.format(i+1), str(p[0]), data)
-				data = re.sub(r'\b02{0}\b'.format(i+1), str(p[1]), data)
-				data = re.sub(r'\b03{0}\b'.format(i+1), str(p[2]), data)
-				data = re.sub(r'\b04{0}\b'.format(i+1), str(p[3]), data)
-				data = re.sub(r'\b05{0}\b'.format(i+1), str(p[4]), data)
-				data = re.sub(r'\b06{0}\b'.format(i+1), str(p[5]), data)
-				data = re.sub(r'\b07{0}\b'.format(i+1), str(p[6]), data)
-				data = re.sub(r'\b08{0}\b'.format(i+1), str(p[7]), data)
-				randomProgress(p)
-			else:
-				data = re.sub(r'\b00{0}\b'.format(i+1), '', data)
-				data = re.sub(r'\b01{0}\b'.format(i+1), '', data)
-				data = re.sub(r'\b02{0}\b'.format(i+1), '', data)
-				data = re.sub(r'\b03{0}\b'.format(i+1), '', data)
-				data = re.sub(r'\b04{0}\b'.format(i+1), '', data)
-				data = re.sub(r'\b05{0}\b'.format(i+1), '', data)
-				data = re.sub(r'\b06{0}\b'.format(i+1), '', data)
-				data = re.sub(r'\b07{0}\b'.format(i+1), '', data)
-				data = re.sub(r'\b08{0}\b'.format(i+1), '', data)
+			for i in range(6):
+				if i+recodeIndex < len(records):
+					data = re.sub(r'\b00{0}\b'.format(i+1), records[i+recodeIndex], data)
+					data = re.sub(r'\b01{0}\b'.format(i+1), str(p[0]), data)
+					data = re.sub(r'\b02{0}\b'.format(i+1), str(p[1]), data)
+					data = re.sub(r'\b03{0}\b'.format(i+1), str(p[2]), data)
+					data = re.sub(r'\b04{0}\b'.format(i+1), str(p[3]), data)
+					data = re.sub(r'\b05{0}\b'.format(i+1), str(p[4]), data)
+					data = re.sub(r'\b06{0}\b'.format(i+1), str(p[5]), data)
+					data = re.sub(r'\b07{0}\b'.format(i+1), str(p[6]), data)
+					data = re.sub(r'\b08{0}\b'.format(i+1), str(p[7]), data)
+					randomProgress(p)
+				else:
+					data = re.sub(r'\b00{0}\b'.format(i+1), '', data)
+					data = re.sub(r'\b01{0}\b'.format(i+1), '', data)
+					data = re.sub(r'\b02{0}\b'.format(i+1), '', data)
+					data = re.sub(r'\b03{0}\b'.format(i+1), '', data)
+					data = re.sub(r'\b04{0}\b'.format(i+1), '', data)
+					data = re.sub(r'\b05{0}\b'.format(i+1), '', data)
+					data = re.sub(r'\b06{0}\b'.format(i+1), '', data)
+					data = re.sub(r'\b07{0}\b'.format(i+1), '', data)
+					data = re.sub(r'\b08{0}\b'.format(i+1), '', data)
 
 		#刪除舊xml
 		remove_from_zip(fileDes.decode('utf8'), 'word/document.xml')
