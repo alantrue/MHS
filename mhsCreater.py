5a7d004c590bf93ae33d0d39c1a41d2879b66a2c
alantrue <alantruekimo@yahoo.com.tw>
2013-11-17 10:22:00 +0800

    utf8 version

--- mhsCreater.py
+++ mhsCreater.py
@@ -8,10 +8,11 @@ import tempfile
 import datetime
 import re
 import random
+import codecs
 
 workPath = os.getcwd() + '/'
 outputPath = workPath + u'病歷表/'
-infoFile = workPath + 'info.csv'
+infoFile = workPath + 'info.txt'
 files = (u'無口語紀錄表.docx', u'無口語評估表.docx', u'詞彙簡單句紀錄表.docx', u'詞彙簡單句評估表.docx', u'構音紀錄表.docx', u'構音評估表.docx', u'複雜句故事敘述紀錄表.docx', u'複雜句故事敘述評估表.docx')
 
 def remove_from_zip(zipfname, *filenames):
@@ -47,58 +48,63 @@ def removeFileInFolder(folder):
 
 def parseInfo():
 	#解析出0病歷號, 1姓名, 2生日, 3初診日, 4性別, 5階段, 6紀錄
-	csvfile = open(infoFile, 'rb')
+	csvfile = open(infoFile, 'r')
 
 	i = 0
-	for row in csv.reader(csvfile, delimiter=',', quotechar='"'):
+	for row in csv.reader(csvfile, delimiter='\t', quotechar='"'):
+		if len(row) != 7:
+			print u'有錯誤'.encode('big5')
+			print u'info.txt 需要 [病歷號] [姓名] [生日] [初診日期] [性別] [階段] [紀錄] 等7個欄位'.encode('big5')
+			break
+
 		fileSrc1 = u''
 		fileSrc2 = u''
-		fileDes1 = u'治療表.zip'.encode('big5')
-		fileDes2 =  u'評估表.zip'.encode('big5')
+		fileDes1 = u'治療表.zip'.encode('utf8')
+		fileDes2 =  u'評估表.zip'.encode('utf8')
 		#依據階段複製範本, 並命名病歷號+姓名+(語言or構音)+(治療, 評估)表
-   		if row[5] == u'無口語'.encode('big5'):
+   		if row[5] == u'無口語'.encode('utf8'):
    			#複製 無口語紀錄表.docx, 無口語評估表.docx
    			fileSrc1 = files[0]
    			fileSrc2 = files[1]
-   			fileDes1 = u'語言'.encode('big5') + fileDes1
-   			fileDes2 = u'語言'.encode('big5') + fileDes2
+   			fileDes1 = u'語言'.encode('utf8') + fileDes1
+   			fileDes2 = u'語言'.encode('utf8') + fileDes2
 
-		elif row[5] == u'簡單句'.encode('big5'):
+		elif row[5] == u'簡單句'.encode('utf8'):
 			#複製 詞彙簡單句紀錄表.docx, 詞彙簡單句評估表.docx
 			fileSrc1 = files[2]
    			fileSrc2 = files[3]
-   			fileDes1 = u'語言'.encode('big5') + fileDes1
-   			fileDes2 = u'語言'.encode('big5') + fileDes2
+   			fileDes1 = u'語言'.encode('utf8') + fileDes1
+   			fileDes2 = u'語言'.encode('utf8') + fileDes2
 
-		elif row[5] == u'構音'.encode('big5'):
+		elif row[5] == u'構音'.encode('utf8'):
 			#複製 構音紀錄表.docx, 構音評估表.docx
 			fileSrc1 = files[4]
    			fileSrc2 = files[5]
-   			fileDes1 = u'構音'.encode('big5') + fileDes1
-   			fileDes2 = u'構音'.encode('big5') + fileDes2
+   			fileDes1 = u'構音'.encode('utf8') + fileDes1
+   			fileDes2 = u'構音'.encode('utf8') + fileDes2
 
-		elif row[5] == u'複雜句'.encode('big5'):
+		elif row[5] == u'複雜句'.encode('utf8'):
 			#複製 複雜句故事敘述紀錄表.docx, 複雜句故事敘述評估表.docx
 			fileSrc1 = files[6]
    			fileSrc2 = files[7]
-   			fileDes1 = u'語言'.encode('big5') + fileDes1
-   			fileDes2 = u'語言'.encode('big5') + fileDes2
+   			fileDes1 = u'語言'.encode('utf8') + fileDes1
+   			fileDes2 = u'語言'.encode('utf8') + fileDes2
 
 		fileDes1 = row[0] + row[1] + fileDes1
 		fileDes2 = row[0] + row[1] + fileDes2
 
-		if fileSrc1 != u'' and fileSrc2 != u'':
+		if fileSrc1 != u''.encode('utf8') and fileSrc2 != u''.encode('utf8'):
 			fileSrc1 = workPath + fileSrc1
 			fileSrc2 = workPath + fileSrc2
-			fileDes1 = outputPath.encode('big5') + fileDes1
-			fileDes2 = outputPath.encode('big5') + fileDes2
+			fileDes1 = outputPath.encode('utf8') + fileDes1
+			fileDes2 = outputPath.encode('utf8') + fileDes2
 
 			#建立拷貝zip
 			try:
-				shutil.copyfile(fileSrc1, fileDes1.decode('big5'))
-				shutil.copyfile(fileSrc2, fileDes2.decode('big5'))
+				shutil.copyfile(fileSrc1, fileDes1.decode('utf8'))
+				shutil.copyfile(fileSrc2, fileDes2.decode('utf8'))
 			except IOError as e:
-				print u'無法產生 {0} {1} 的病歷表'.encode('big5').format(row[0], row[1])
+				print u'無法產生 {0} 的病歷表'.encode('big5').format(row[0])
 				print u'進行中'.encode('big5')
 				continue
 
@@ -110,17 +116,17 @@ def parseInfo():
 
 def createMHS(row, fileDes):	
 	#解壓縮
-		zfr = zipfile.ZipFile(fileDes.decode('big5'), 'r')
+		zfr = zipfile.ZipFile(fileDes.decode('utf8'), 'r')
 		data = zfr.read('word/document.xml')
 		data = data.decode('utf8')
 	#填入資料
 		#病歷號碼
-		data = re.sub(r'\b123456\b', row[0].decode('big5'), data)
+		data = re.sub(r'\b123456\b', row[0].decode('utf8'), data)
 
 		#名字
 		replaceLen = len(u'我的名字')
-		nameLen = len(row[1].decode('big5'))
-		replaceName = row[1].decode('big5') + u'　' * (replaceLen - nameLen)
+		nameLen = len(row[1].decode('utf8'))
+		replaceName = row[1].decode('utf8') + u'　' * (replaceLen - nameLen)
 		data = data.replace(u'我的名字', replaceName)
 
 		#生日
@@ -128,18 +134,18 @@ def createMHS(row, fileDes):
 		if birth[0] == '0':
 			birth = birth[1:]
 
-		data = re.sub(r'\bbirth\b', birth.decode('big5'), data)
+		data = re.sub(r'\bbirth\b', birth.decode('utf8'), data)
 
 		#初診日期
-		firstDate = row[3].decode('big5').split('/')
+		firstDate = row[3].decode('utf8').split('/')
 		data = re.sub(r'\bjkl\b', firstDate[0], data)
 		data = re.sub(r'\bmn\b', firstDate[1], data)
 		data = re.sub(r'\bop\b', firstDate[2], data)
 
 		#男女
-		if row[4] == u'男'.encode('big5'):
+		if row[4] == u'男'.encode('utf8'):
 			data = replaceOccurence(data, '<w:default w:val="1"/>', '', 1)
-		elif row[4] == u'女'.encode('big5'):
+		elif row[4] == u'女'.encode('utf8'):
 			data = replaceOccurence(data, '<w:default w:val="1"/>', '', 0)
 		else:
 			pass
@@ -172,19 +178,19 @@ def createMHS(row, fileDes):
 				data = re.sub(r'\b08{0}\b'.format(i+1), '', data)
 
 		#刪除舊xml
-		remove_from_zip(fileDes.decode('big5'), 'word/document.xml')
+		remove_from_zip(fileDes.decode('utf8'), 'word/document.xml')
 
 		#寫入新xml
-		zfw = zipfile.ZipFile(fileDes.decode('big5'), 'a')
+		zfw = zipfile.ZipFile(fileDes.decode('utf8'), 'a')
 		zfw.writestr('word/document.xml', data.encode('utf8'))
 
 		zfr.close()
 		zfw.close()
 	#改回docx
-		filename = os.path.splitext(fileDes.decode('big5'))[0]
-		os.rename(fileDes, filename + ".docx")
+		filename = os.path.splitext(fileDes.decode('utf8'))[0]
+		os.rename(fileDes.decode('utf8'), filename + ".docx")
 
 print u'進行中'.encode('big5')
 removeFileInFolder(outputPath)
 parseInfo()
-print u'完成'.encode('big5')
+print u'結束'.encode('big5')
